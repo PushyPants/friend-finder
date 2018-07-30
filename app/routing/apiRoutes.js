d3fb164020f399e9f37b41aa06984e84a9a34bba
Push <crashjohnson@gmail.com>
2018-07-29 19:32:34 -0500

    got the function to calulate the answers working to console the information in the node cli

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,8 +11,42 @@ module.exports = function(app){
 
     app.post('/api/friends', function(req, res) {
         let newFriend = req.body;
+        let diffArr = [];
         res.json(newFriend);
         friends.newFriend.push(newFriend)
+     
+        //quick function to find the difference between two values
+        function diff(a,b) {
+            return Math.abs(a-b);
+        };
+        
+        //loop through each potential match
+        friends.friendData.forEach(function(element){
+            let diffScore = 0;
+
+            //loop through each answer and find the difference
+            element.scores.forEach(function(score, index){
+                let a = parseInt(score);
+                let b = parseInt(friends.newFriend[0].scores[index])
+
+                diffScore += diff(a,b)
+            })
+
+            //push the difference to the array of differences
+            diffArr.push(diffScore);
+        })
+
+        console.log(diffArr)
+        console.log(Math.min(...diffArr))
+
+        let lowestVal = Math.min(...diffArr);
+        let lowIndex = diffArr.indexOf(lowestVal)
+        console.log(`index of ${lowestVal} is ${lowIndex}`)
+        let friendMatch = friends.friendData[lowIndex];
+
+        console.log(friendMatch.name)
+
+
     })
 
 
